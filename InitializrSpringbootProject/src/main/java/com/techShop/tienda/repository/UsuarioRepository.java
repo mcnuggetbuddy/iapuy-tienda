27719e6044225b48c85cb5f34a828bd144ca55a5
mcnuggetbuddy <apuyisrael@gmail.com>
2026-03-23 20:26:58 -0600

    Avance Semana 10
    Se modificaron los usuarios y rutas. Se modificó el SecurityConfig para trabajar con la BD

---
+++ InitializrSpringbootProject/src/main/java/com/techShop/tienda/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package com.techShop.tienda.repository;
+
+import com.techShop.tienda.domain.Usuario;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    
+    public Optional<Usuario> findByUsernameAndActivoTrue(String username);
+    
+}
